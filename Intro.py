86e3be4d84b0e0960e6d653b1f327eaee54033b4
Arnav <ajohri2002@gmail.com>
2024-04-21 14:27:49 +0530

    solved

---
+++ Intro.py
@@ -0,0 +1,7 @@
+from tkinter import X, Y
+
+
+a = [1,2,3]
+b = [X,Y]
+a.extend(b)
+print(a)
---
+++ script.js
@@ -0,0 +1,7 @@
+let arr = [1,2,3,4,5];
+let sum = 0;
+for (let index = 0; index < arr.length; index++) {
+    sum += arr[index];
+}
+
+console.log(sum);
